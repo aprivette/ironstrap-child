5751677592ee5dfd9e308b6bc0362910f3dad7a3
Adam Privette <aprivette@gmail.com>
2017-06-19 15:38:42 -0400

    added —production flag to minify assets

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,32 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cleancss = require('gulp-clean-css');
-var order = require('gulp-order');
+var gulpif = require('gulp-if');
+var argv = require('yargs').argv;
+
+// Check if compiling with production flag
+var isProduction = (argv.production === undefined) ? false : true;
 
 // Compile Sass
 gulp.task('sass', function() {
     return gulp.src('sass/site.scss')
         .pipe(sass())
-        .pipe(gulp.dest('css'))
-        .pipe(concat('site.css'))
-        .pipe(cleancss())
+        .pipe(gulpif(isProduction, cleancss()))
         .pipe(rename('site.min.css'))
-        .pipe(gulp.dest('css/min'));
+        .pipe(gulp.dest('css'));
 });
 
 // List all javascript files and directories to include in the correct order.
 var sourceJs = [
-    //'node_modules/example-pkg/example/example.min.js',
-    'js/*.js',
+	///'node_modules/example-pkg/example/example.min.js',
+	'js/*.js',
 ];
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
     return gulp.src(sourceJs)
-        .pipe(concat('site.js'))
-        .pipe(uglify())
+    	.pipe(concat('site.js'))
+        .pipe(gulpif(isProduction, uglify()))
         .pipe(rename('site.min.js'))
         .pipe(gulp.dest('js/min'));
 });
