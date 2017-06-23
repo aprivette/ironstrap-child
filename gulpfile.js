ff32b8e05d4882a425d20021deff61755f8d754b
Adam Privette <aprivette@gmail.com>
2017-06-23 14:25:59 -0400

    added gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cleancss = require('gulp-clean-css');
 var gulpif = require('gulp-if');
+var autoprefixer = require('gulp-autoprefixer');
 var argv = require('yargs').argv;
 
 // Check if compiling with production flag
@@ -15,20 +16,24 @@ gulp.task('sass', function() {
     return gulp.src('sass/site.scss')
         .pipe(sass())
         .pipe(gulpif(isProduction, cleancss()))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(rename('site.min.css'))
         .pipe(gulp.dest('css'));
 });
 
 // List all javascript files and directories to include in the correct order.
 var sourceJs = [
-	///'node_modules/example-pkg/example/example.min.js',
-	'js/*.js',
+    ///'node_modules/example-pkg/example/example.min.js',
+    'js/*.js',
 ];
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
     return gulp.src(sourceJs)
-    	.pipe(concat('site.js'))
+        .pipe(concat('site.js'))
         .pipe(gulpif(isProduction, uglify()))
         .pipe(rename('site.min.js'))
         .pipe(gulp.dest('js/min'));
